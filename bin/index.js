0a2d917d89f9f51c23df3a9950ea3931f8c33720
Patrick Martin <patrickwaltermartin@gmail.com>
2024-05-08 18:25:25 -0300

    Add usage function to display usage text
    Added a function to display the cli usage text currently set up to display when no command is entered

--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,20 @@
 #! /usr/bin/env node
-console.log(process.argv);
+const args = process.argv;
 
+const usage = function(){
+    const usageText = `
+    mypm <command>
+
+    Usage:
+
+    mypm install    <package_name> add a package dependancy to your project
+    mypm publish    publish a package to the mypm registry
+    mypm init       create a myPackage.json file 
+    `
+    console.log(usageText);
+}
+
+const commands = ['init', 'install', 'publish']
+if (args.length < 3){
+    usage();
+}
