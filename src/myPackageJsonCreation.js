1653d77b1fd9dc70b25f70806655ee704fb97531
Patrick Martin <patrickwaltermartin@gmail.com>
2024-05-09 20:12:28 -0300

    Change init text to use mypm instead of init

--- src/myPackageJsonCreation.js
+++ src/myPackageJsonCreation.js
@@ -9,10 +9,10 @@ const myPackageJsonQuestions = function(){
 This utility will walk you through creating a package.json file.
 It only covers the most common items, and tries to guess sensible defaults.
 
-See \`npm help init\` for definitive documentation on these fields
+See \`mypm help init\` for definitive documentation on these fields
 and exactly what they do.
 
-Use \`npm install <pkg>\` afterwards to install a package and
+Use \`mypm install <pkg>\` afterwards to install a package and
 save it as a dependency in the package.json file.
 
 Press ^C at any time to quit.`);
