58be0e4ed1469fa32308efcbf1315bdee57fa426
Patrick Martin <patrickwaltermartin@gmail.com>
2024-05-09 20:46:06 -0300

    Add the rest of the basic question except for keywords

--- src/myPackageJsonCreation.js
+++ src/myPackageJsonCreation.js
@@ -5,6 +5,8 @@ import { basename } from 'path';
 const prompt = promptSync();
 
 const myPackageJsonQuestions = function(){
+    const myPackageJsonAwnsers = {};
+
     console.log(`
 This utility will walk you through creating a package.json file.
 It only covers the most common items, and tries to guess sensible defaults.
@@ -16,11 +18,50 @@ Use \`mypm install <pkg>\` afterwards to install a package and
 save it as a dependency in the package.json file.
 
 Press ^C at any time to quit.`);
+
     const cwd = basename(process.cwd());
     const packageName = prompt(`package name: (${cwd})`);
-    console.log(packageName);
     
+    if (packageName === ''){
+        myPackageJsonAwnsers.title = cwd;
+    } else {
+        myPackageJsonAwnsers.title = packageName;
+    }
 
+    const version = prompt('version: (1.0.0)');
+    if (version === ''){
+        myPackageJsonAwnsers.version = '1.0.0';
+    } else{
+        myPackageJsonAwnsers.version = version;
+    } 
+
+    const description = prompt('description:');
+    myPackageJsonAwnsers.desription = description;
+    
+    //Add the keyword logic later
+    myPackageJsonAwnsers.keywords = [];
+
+    const author = prompt('author:');
+    myPackageJsonAwnsers.author = author;
+
+    const license = prompt('license (ISC);');
+    if (license === ''){
+        myPackageJsonAwnsers.license = 'ISC';
+    } else{
+        myPackageJsonAwnsers.license = license;
+    }
+    console.log(`About to write to ${process.cwd()}package.json:`)
+    console.log();
+    console.log(myPackageJsonAwnsers);
+
+    const isOk = prompt('Is this OK? (yes)');
+    //npm at least in version 10.2.3 only checks if the first letter of the ok prompt response is y
+    if (isOk !== '' && isOk[0] !== 'y'){
+        console.log('Aborted');
+        process.exit(1);
+    } 
+    
+    return myPackageJsonAwnsers;
 }
 
 export const createMyPackageJsonFile = function(){
